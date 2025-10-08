79cd15d026ad0f20a34a4f6025236795b45ac52a
Kandy-Hamisi <hkandy2000@gmail.com>
2025-10-08 08:42:15 +0300

    Finalized header

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const NAV_ITEMS = [
   { href: "/", title: "Dashboard" },
-  { href: "/", title: "Dashboard" },
-  { href: "/", title: "Dashboard" },
-  { href: "/", title: "Dashboard" },
+  { href: "/search", title: "Search" },
+  { href: "/stocks", title: "Stocks" },
+  { href: "/community", title: "Community" },
 ];
