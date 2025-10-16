138543dcc26514a0e362d1f1bff6cb98fe2a7394
General Fib <61687064+Kandy-Hamisi@users.noreply.github.com>
2025-10-16 10:01:23 +0300

    Fix: Added comma at end of cron job

--- lib/ingest/functions.ts
+++ lib/ingest/functions.ts
@@ -65,7 +65,7 @@ export const sendSignUpEmail = inngest.createFunction(
 export const sendDailyNewsSummary = inngest.createFunction(
   { id: "daily-news-summary" },
   // [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }],
-  [{ event: "app/send.daily.news" }, { cron: "0 12 */7 * *" }]
+  [{ event: "app/send.daily.news" }, { cron: "0 12 */7 * *" }],
   // [{ event: "app/send.daily.news" }, { cron: "*/2 * * * *" }],
   async ({ step }) => {
     // Step #1: Get all users for news delivery
